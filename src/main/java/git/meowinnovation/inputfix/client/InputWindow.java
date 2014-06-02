5ba947c7748bd52ae40262fa5d2435ab9f02e9e0
Brian Zhou <Brian1461748123@gmail.com>
2014-06-02 03:48:19 -0700

    input window no frame

--- src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
+++ src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
@@ -1,6 +1,10 @@
 package git.meowinnovation.inputfix.client;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -23,16 +27,22 @@ import javax.swing.JToggleButton;
 import javax.swing.KeyStroke;
 
 public class InputWindow {
-
-	public static void showGUI() {
+ 
+	
+    public static void showGUI() {
 
 		final JFrame frame = new JFrame("Input Window");
 		final JTextArea comp = new JTextArea();
-
+		
+		
+	
 		frame.getContentPane().add(comp, BorderLayout.CENTER);
 		frame.setSize(288, 40);
+		frame.setUndecorated(true);
 		frame.setVisible(true);
 		comp.requestFocus();
+		
+		
 		
 		KeyStroke enter = KeyStroke
 				.getKeyStroke(KeyEvent.VK_ENTER, 0);
@@ -65,5 +75,7 @@ public class InputWindow {
 		actionmap.put("enter", enteraction);
 		actionmap.put("esc", escaction);
 	}
+    
+    
 
 }
