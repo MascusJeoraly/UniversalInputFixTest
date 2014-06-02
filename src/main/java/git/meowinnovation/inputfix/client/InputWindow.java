5c17a9877cdc721e066125829beb5d3c3729a8d5
Brian Zhou <Brian1461748123@gmail.com>
2014-06-02 03:08:10 -0700

    尝试增加enter热键

--- src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
+++ src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -21,9 +22,13 @@ public class InputWindow {
 	public static void showGUI() {
 		JFrame frame = new JFrame("Input Window");
 		JComponent comp = new JTextArea();
+		JButton enter = new JButton("233");
+		enter.setMnemonic(KeyEvent.VK_ENTER);
 		frame.getContentPane().add(comp, BorderLayout.CENTER);
 		frame.setSize(288, 40);
+		enter.setSize(20, 40);
 		frame.setVisible(true);
+		enter.setVisible(true);
 		comp.requestFocus();
 	}
 
