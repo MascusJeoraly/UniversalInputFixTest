1f1462b6a35c6c090f75f2b338280f4ce3df92ba
Fang <fang0716fy@gmail.com>
2014-06-02 18:39:48 +0800

    key -> event

--- src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
+++ src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
@@ -1,52 +1,74 @@
+/*
+ * Copyright (C) 2014  Fang0716
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package git.meowinnovation.inputfix.client;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
+import cpw.mods.fml.client.FMLClientHandler;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.ButtonGroup;
-import javax.swing.InputMap;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
-import javax.swing.JToggleButton;
-import javax.swing.KeyStroke;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class InputWindow {
 
-	public static void showGUI() {
-		JFrame frame = new JFrame("Input Window");
-		final JTextArea comp = new JTextArea();
-		KeyStroke enter_keystroke = KeyStroke
-				.getKeyStroke(KeyEvent.VK_ENTER, 0);
+    public static void showGUI() {
 
-		frame.getContentPane().add(comp, BorderLayout.CENTER);
-		frame.setSize(288, 40);
-		frame.setVisible(true);
-		comp.requestFocus();
+        final JFrame frame = new JFrame("Input Window");
+        final JTextArea comp = new JTextArea();
 
-		Action send_message_then_clear_action = new AbstractAction() {
+        frame.getContentPane().add(comp, BorderLayout.CENTER);
+        frame.setSize(288, 40);
+        frame.setVisible(true);
+        comp.requestFocus();
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println(comp.getText());
-			}
+        KeyStroke enter = KeyStroke
+                .getKeyStroke(KeyEvent.VK_ENTER, 0);
+        KeyStroke esc = KeyStroke
+                .getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+        InputMap inputmap = comp.getInputMap();
+        ActionMap actionmap = comp.getActionMap();
 
-		};
-		InputMap inputmap = comp.getInputMap();
-		ActionMap actionmap = comp.getActionMap();
-		inputmap.put(enter_keystroke,"send-message-then-clear");
-		actionmap.put("send-message-then-clear",send_message_then_clear_action);
-	}
+        Action enteraction = new AbstractAction() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("enter");
+            }
+
+        };
+
+        Action escaction = new AbstractAction() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                frame.dispose();
+            }
+
+        };
+
+
+        inputmap.put(enter, "enter");
+        inputmap.put(esc, "esc");
+        actionmap.put("enter", enteraction);
+        actionmap.put("esc", escaction);
+        //TODO replace it
+        FMLClientHandler.instance().getClient().thePlayer.sendChatMessage("test");
+    }
+
 
 }
