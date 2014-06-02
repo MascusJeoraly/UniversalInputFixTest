3f622b462e1afe590bede96fbf92f8d6efeca1ee
Brian Zhou <Brian1461748123@gmail.com>
2014-06-02 03:16:56 -0700

    add enter control key

--- src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
+++ src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
@@ -7,7 +7,11 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
 import javax.swing.ButtonGroup;
+import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
@@ -16,20 +20,33 @@ import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
+import javax.swing.KeyStroke;
 
 public class InputWindow {
 
 	public static void showGUI() {
 		JFrame frame = new JFrame("Input Window");
-		JComponent comp = new JTextArea();
-		JButton enter = new JButton("233");
-		enter.setMnemonic(KeyEvent.VK_ENTER);
+		final JTextArea comp = new JTextArea();
+		KeyStroke enter_keystroke = KeyStroke
+				.getKeyStroke(KeyEvent.VK_ENTER, 0);
+
 		frame.getContentPane().add(comp, BorderLayout.CENTER);
 		frame.setSize(288, 40);
-		enter.setSize(20, 40);
 		frame.setVisible(true);
-		enter.setVisible(true);
 		comp.requestFocus();
+
+		Action send_message_then_clear_action = new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(comp.getText());
+			}
+
+		};
+		InputMap inputmap = comp.getInputMap();
+		ActionMap actionmap = comp.getActionMap();
+		inputmap.put(enter_keystroke,"send-message-then-clear");
+		actionmap.put("send-message-then-clear",send_message_then_clear_action);
 	}
 
 }
