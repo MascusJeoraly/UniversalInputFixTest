a09c8a28a65cc80dc94dce8b31353870ca3f949e
Brian Zhou <Brian1461748123@gmail.com>
2014-06-02 02:57:31 -0700

    _( : 3 」∠ )_forgot to save files

--- src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
+++ src/main/java/git/meowinnovation/inputfix/client/InputWindow.java
@@ -1,5 +1,30 @@
 package git.meowinnovation.inputfix.client;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
+
 public class InputWindow {
+
+	public static void showGUI() {
+		JFrame frame = new JFrame("Input Window");
+		JComponent comp = new JTextArea();
+		frame.getContentPane().add(comp, BorderLayout.CENTER);
+		frame.setSize(288, 40);
+		frame.setVisible(true);
+		comp.requestFocus();
+	}
 
 }
